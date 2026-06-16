aab95401eacf8586333453c74259c390dac27801
Alex-r6 <oorlov.1986@gmail.com>
2026-06-16 14:33:54 +0300

    _commit

--- src/components/Home/Home_item.jsx
+++ src/components/Home/Home_item.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Change_count_product } from '../Change_count_product/Change_count_product'
+import { Context_menu } from '../Context_menu/Context_menu'
 
 export const Home_item = ({elem, colors_rd, show_to_backet, backet, to_backet, setTo_backet, remove_product}) => {
     return (
-        <div key={elem.id} className='prod_item' style={{ display: 'flex', gap: '15px', background: colors_rd.productBG }} onContextMenu={(e) => show_to_backet(e, elem.id)}>
+        <div  className='prod_item' style={{ display: 'flex', gap: '15px', background: colors_rd.productBG }} onContextMenu={(e) => show_to_backet(e, elem.id)}>
             <p style={{ color: colors_rd.nameColor, fontSize: `${colors_rd.nameFontSize}px` }}>{elem.name}</p>
             <p style={{ color: colors_rd.priceColor, fontSize: `${colors_rd.priceFontSize}px` }}>{elem.price}</p>
             <p style={{ color: colors_rd.categoryColor, font: `${colors_rd.categoryFontSize}px` }}>{elem.category}</p>
